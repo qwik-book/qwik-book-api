a45e392424939d600bc258bb64bdf753ad2b3fbf
anartzdev <105705996+anartzdev@users.noreply.github.com>
2024-01-08 12:15:37 +0100

    feat: add new characters

--- db/characters.js
+++ db/characters.js
@@ -65,6 +65,110 @@ const characters = [
       url: 'https://breakingbad.fandom.com/wiki/Tuco_Salamanca',
       votes: 7,
     },
+    {
+        "id": 9,
+        "name": "Gale Boetticher",
+        "description": "Químico y colaborador de Gustavo Fring.",
+        "episodes": 14,
+        "url": "https://breakingbad.fandom.com/wiki/Gale_Boetticher",
+        "votes": 7
+      },
+      {
+        "id": 10,
+        "name": "Lydia Rodarte-Quayle",
+        "description": "Distribuidora de metilamina y asociada de Gus Fring.",
+        "episodes": 20,
+        "url": "https://breakingbad.fandom.com/wiki/Lydia_Rodarte-Quayle",
+        "votes": 6
+      },
+      {
+        "id": 11,
+        "name": "Todd Alquist",
+        "description": "Ayudante de Jack Welker y asesino a sueldo.",
+        "episodes": 25,
+        "url": "https://breakingbad.fandom.com/wiki/Todd_Alquist",
+        "votes": 6
+      },
+      {
+        "id": 12,
+        "name": "Jack Welker",
+        "description": "Líder de una banda de delincuentes.",
+        "episodes": 14,
+        "url": "https://breakingbad.fandom.com/wiki/Jack_Welker",
+        "votes": 5
+      },
+      {
+        "id": 13,
+        "name": "Jane Margolis",
+        "description": "Novia de Jesse Pinkman y artista.",
+        "episodes": 12,
+        "url": "https://breakingbad.fandom.com/wiki/Jane_Margolis",
+        "votes": 5
+      },
+      {
+        "id": 14,
+        "name": "Skinny Pete",
+        "description": "Amigo de Jesse Pinkman y traficante de drogas.",
+        "episodes": 19,
+        "url": "https://breakingbad.fandom.com/wiki/Skinny_Pete",
+        "votes": 4
+      },
+      {
+        "id": 15,
+        "name": "Badger",
+        "description": "Amigo de Jesse Pinkman y traficante de drogas.",
+        "episodes": 16,
+        "url": "https://breakingbad.fandom.com/wiki/Badger",
+        "votes": 4
+      },
+      {
+        "id": 16,
+        "name": "Huell Babineaux",
+        "description": "Guardaespaldas y ayudante de Saul Goodman.",
+        "episodes": 14,
+        "url": "https://breakingbad.fandom.com/wiki/Huell_Babineaux",
+        "votes": 3
+      },
+      {
+        "id": 17,
+        "name": "Marie Schrader",
+        "description": "Esposa de Hank Schrader y obsesionada por el color morado.",
+        "episodes": 62,
+        "url": "https://breakingbad.fandom.com/wiki/Marie_Schrader",
+        "votes": 4
+      },
+      {
+        "id": 18,
+        "name": "Ted Beneke",
+        "description": "Dueño de una empresa de servicios públicos y ex-amante de Skyler White.",
+        "episodes": 17,
+        "url": "https://breakingbad.fandom.com/wiki/Ted_Beneke",
+        "votes": 3
+      },
+      {
+        "id": 19,
+        "name": "Donald Margolis",
+        "description": "Controlador de tráfico aéreo y padre de Jane Margolis.",
+        "episodes": 4,
+        "url": "https://breakingbad.fandom.com/wiki/Donald_Margolis",
+        "votes": 3
+      },
+      {
+        "id": 20,
+        "name": "Gretchen Schwartz",
+        "description": "Co-fundadora de Gray Matter Technologies y ex-pareja de Walter White.",
+        "episodes": 7,
+        "url": "https://breakingbad.fandom.com/wiki/Gretchen_Schwartz",
+        "votes": 2
+      },
+      {
+        "id": 21,
+        "name": "Elliot Schwartz",
+        "description": "Co-fundador de Gray Matter Technologies y ex-compañero de Walter White.",
+        "episodes": 6,
+        "url": "https://breakingbad.fandom.com/wiki/Elliot_Schwartz",
+        "votes": 2
+      }
   ];
 
   module.exports = { characters };
